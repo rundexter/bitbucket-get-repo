c824d4d4921e38f13bf14cda08e53c0c56fab9fa
Alexander Zaretskii <al.zaretskii@gmail.com>
2015-12-11 14:10:22 +0200

    add DEXTER module

--- index.js
+++ index.js
@@ -1,4 +1,161 @@
+var _ = require('lodash');
+
+var request = require('request').defaults({
+    baseUrl: 'https://api.bitbucket.org/2.0/'
+});
+
+var globalPickResult = {
+    'description': 'description',
+    'name': 'name',
+    'updated_on': 'updated_on',
+    'uuid': 'uuid',
+    'links_html': 'links.html.href',
+    'owner_username': 'owner.username',
+    'owner_uuid': 'owner.uuid'
+};
+
 module.exports = {
+
+    /**
+     * Return pick result.
+     *
+     * @param output
+     * @param pickTemplate
+     * @returns {*}
+     */
+    pickResult: function (output, pickTemplate) {
+        var result = _.isArray(pickTemplate)? [] : {};
+        // map template keys
+        _.map(pickTemplate, function (templateValue, templateKey) {
+
+            var outputValueByKey = _.get(output, templateValue.keyName || templateValue, undefined);
+
+            if (_.isUndefined(outputValueByKey)) {
+
+                result = _.isEmpty(result)? undefined : result;
+                return;
+            }
+
+
+            // if template key is object - transform, else just save
+            if (_.isArray(pickTemplate)) {
+
+                result = outputValueByKey;
+            } else if (_.isObject(templateValue)) {
+                // if data is array - map and transform, else once transform
+                if (_.isArray(outputValueByKey)) {
+                    var mapPickArrays = this._mapPickArrays(outputValueByKey, templateKey, templateValue);
+
+                    result = _.isEmpty(result)? mapPickArrays : _.merge(result, mapPickArrays);
+                } else {
+
+                    result[templateKey] = this.pickResult(outputValueByKey, templateValue.fields);
+                }
+            } else {
+
+                _.set(result, templateKey, outputValueByKey);
+            }
+        }, this);
+
+        return result;
+    },
+
+    /**
+     * System func for pickResult.
+     *
+     * @param mapValue
+     * @param templateKey
+     * @param templateObject
+     * @returns {*}
+     * @private
+     */
+    _mapPickArrays: function (mapValue, templateKey, templateObject) {
+        var arrayResult = [],
+            result = templateKey === '-'? [] : {};
+
+        _.map(mapValue, function (inOutArrayValue) {
+            var pickValue = this.pickResult(inOutArrayValue, templateObject.fields);
+
+            if (pickValue !== undefined)
+                arrayResult.push(pickValue);
+        }, this);
+
+        if (templateKey === '-') {
+
+            result = arrayResult;
+        } else {
+
+            result[templateKey] = arrayResult;
+        }
+
+        return result;
+    },
+
+    authParams: function (dexter) {
+        var auth = {},
+            username = dexter.environment('bitbucket_username'),
+            password = dexter.environment('bitbucket_password');
+
+        if (username && password) {
+
+            auth.user = username;
+            auth.pass = password;
+        }
+
+        return _.isEmpty(auth)? false : auth;
+    },
+
+    /**
+     * Send api request.
+     *
+     * @param method
+     * @param api
+     * @param options
+     * @param auth
+     * @param callback
+     */
+    apiRequest: function (method, api, options, auth, callback) {
+
+        request[method]({url: api, qs: options, auth: auth, json: true}, callback);
+    },
+
+    processResult: function (error, responce, body) {
+
+        if (error)
+
+            this.fail(error);
+
+        else if (responce && !body)
+
+            this.fail(responce.statusCode + ': Something is happened');
+
+        else if (responce && body.error)
+
+            this.fail(responce.statusCode + ': ' + JSON.stringify(body.error));
+
+        else
+
+            this.complete(this.pickResult(body, globalPickResult));
+    },
+
+    checkCorrectParams: function (auth, owner, repo_slug) {
+        var result = true;
+
+        if (!auth) {
+
+            result = false;
+            this.fail('A [bitbucket_username, bitbucket_password] environment need for this module.');
+        }
+
+        if (!step.input('owner').first() || !step.input('repo_slug').first()) {
+
+            result = false;
+            this.fail('A [owner, repo_slug] inputs need for this module.');
+        }
+
+        return result;
+    },
+
     /**
      * The main entry point for the Dexter module
      *
@@ -6,8 +163,17 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+
+        var auth = this.authParams(dexter),
+            owner = step.input('owner').first(),
+            repo_slug = step.input('repo_slug').first();
+
+        // check params.
+        if (!this.checkCorrectParams(auth, step)) return;
+        //send API request
+        this.apiRequest('get', 'repositories/' + owner + '/' + repo_slug, {}, auth, function (error, responce, body) {
+
+            this.processResult(error, responce, body);
+        }.bind(this));
     }
 };
